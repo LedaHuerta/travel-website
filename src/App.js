5aa5430798610fd0a3b3280a127260e6e1c59180
Leda Michelle Huerta Recinas <ledamhuerta@gmail.com>
2021-08-18 00:23:25 -0500

    modifing routes

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/travel-website/home" exact component={Home} />
-          <Route path="/travel-website/services" exact component={Services} />
-          <Route path="/travel-website/products" exact component={Products} />
-          <Route path="/travel-website/sign-up" exact component={SignUp} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/services" exact component={Services} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/sign-up" exact component={SignUp} />
         </Switch>
         <Footer />
       </Router>
